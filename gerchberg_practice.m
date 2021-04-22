60352fb96e1da5368c5ed2ae6a76f0263c559f91
mvchalupnik <mvchalupnik@gmail.com>
2021-04-22 12:56:52 -0400

    plot improvements and cleanup

--- gerchberg_practice.m
+++ gerchberg_practice.m
@@ -1,34 +1,17 @@
 % Practicing Gerchberg-Saxton algorithm for determining phases from
 % phase-less, intensity-only "nearfield" and "farfield" images
 
-fileloc = 'matlab-practice';
-
+fileloc = 'gs-practice2';
 
 if ~exist(fileloc, 'dir')
    mkdir(fileloc) 
 end
 
+
 % First, generate our nearfield image using Matlab's built-in peaks
-% function: 
+% function. Also, multiply by some complex phases ts
 P = peaks(20);
-X = repmat(P,[5 10]);
-%save_imgs(X, [fileloc, '/nf1']);
-
-% Next, take fourier transform to get farfield image.
-%save_imgs(abs(fftshift(fft2(X))), [fileloc, '/ff1']);
-
-
-
-
-%Say we only had the intensity of the farfield, and the intensity in nearfield, 
-%but we wanted to reconstruct the phases necessary in nearfield to obtain 
-%the intensity produced in farfield. 
-trg = abs(fft2(X)); %no fftshift (?), target (we got rid of the phases
-%save_imgs(abs(fftshift(fft2(X))), [fileloc, '/trg_meas_ff']);
-
-%Set source equal to the "nearfield" peaks pattern multiplied by some 
-%arbitrarily chosen phase (since we can't measure the phase)
-src = X; 
+src = repmat(P,[5 10]);
 ts = ones(20,20);
 
 for m=1:4
@@ -39,42 +22,77 @@ for m=1:9
    ts = [ts, ones(100,20)*exp(i*pi*m/5)]; 
 end
 
-src = src.*ts; %ts is what we are trying to "guess" from the GS algorithm
+%ts (complex phases) are what we are trying to retrieve from the GS algorithm
+src = src.*ts; 
 
 
-% target
-trg = abs(fft2(src));
-save_imgs(abs(fftshift(fft2(X))), [fileloc, '/trg_meas_ff'], 'abs(farfield)');
+%Say we only had the intensity of the farfield, and the intensity in nearfield, 
+%but we wanted to reconstruct the phases necessary in nearfield to obtain 
+%the intensity produced in farfield. 
 
-% src
-src = abs(src);
-save_imgs(abs(fftshift(fft2(X))), [fileloc, '/src_meas_nf'], 'abs(nearfield)');
+% Save the amplitude of the target
+trg_abs = abs(fft2(src));
+trg_abs = abs(trg_abs)/max(max(abs(trg_abs)));
+save_imgs(trg_abs, [fileloc, '/trg_abs_ff'], 'Abs(farfield)');
+
+%Save phase map of source, to compare at the end
+save_imgs(angle(src), [fileloc, '/src_angle'], 'Arg(nearfield)', 'jet')
+
+% Save amplitude of src
+src_abs = abs(src)/max(max(abs(src)));
+save_imgs(src_abs, [fileloc, '/src_abs_nf'], 'Abs(nearfield)');
 
 
 %% Gerchberg-Saxton algorithm, following: 
 % https://en.wikipedia.org/wiki/Gerchberg%E2%80%93Saxton_algorithm
-A = ifft2(trg);
+
+%Start by taking the inverse FT of our phase-less target image
+A = ifft2(trg_abs);
+save_imgs(A, [fileloc, '/A_iter', num2str(1)],['A iter. ', num2str(1)]);
+
+%Save phase map at step 1
+save_imgs(angle(A), [fileloc, '/src_angle_1'], ...
+    ['Arg(nearfield) iter. ', num2str(1)], 'jet');
 
 
-for k=1:2%10
-    B = abs(src).*exp(i*angle(A));
+for k=1:1
+%while sum(sum(abs(A - src))) > 1e4
+    B = abs(src_abs).*exp(i*angle(A));
     save_imgs(B, [fileloc, '/B_iter', num2str(k)], ['B iter. ', num2str(k)]);
+    
     C = fft2(B);
-    save_imgs(C, [fileloc, '/C_iter', num2str(k)],['C iter.', num2str(k)]);
-    D = abs(trg).*exp(i*angle(C));
-    save_imgs(D, [fileloc, '/D_iter', num2str(k)],['D iter.', num2str(k)]);
+    save_imgs(C, [fileloc, '/C_iter', num2str(k)],['C iter. ', num2str(k)]);
+    
+    D = abs(trg_abs).*exp(i*angle(C));
+    save_imgs(D, [fileloc, '/D_iter', num2str(k)],['D iter. ', num2str(k)]);
+    
     A = ifft2(D);
-    save_imgs(A, [fileloc, '/A_iter', num2str(k)],['A iter.', num2str(k)]);
+    save_imgs(A, [fileloc, '/A_iter', num2str(k+1)],['A iter. ', num2str(k+1)]);
+    
+    %Save phase map at step k
+    save_imgs(angle(A), [fileloc, '/src_angle_', num2str(k+1)],...
+        ['Arg(nearfield) iter. ', num2str(k+1)], 'jet');
+
     
 end
 
+%Save phase map of source
+save_imgs(angle(A), [fileloc, '/src_angle_recon'], 'Reconstructed Arg(nearfield)')
 
 
 
-function [] = save_imgs(data, savepath, ttl)
+function [] = save_imgs(data, savepath, ttl, cmap)
     hdl = figure;
+    
+    if nargin > 3
+        colormap(hdl, cmap);
+    end
+    
     imagesc(abs(data)/max(max(abs(data))));
-    title(ttl);
+    title(ttl, 'FontSize', 24);
+    xticks([]);
+    yticks([]);
+    
     saveas(hdl, [savepath, '.png']);
     save([savepath, '.mat'], 'data');
     close(hdl);
